34f640e0da0cf152e3bd478fb66281e88736f639
Gino <grivera@canvia.com>
2021-04-24 18:04:58 -0500

    cambio

---
+++ src/test/java/pe/interbank/SeleniumTest.java
@@ -0,0 +1,40 @@
+package pe.interbank;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    public  String baseUrl = "https://www.google.com/";
+    public  WebDriver driver;
+
+    @BeforeTest
+    public  void setBaseUrl() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\xt9645\\Desktop\\CapacitacionBDDJaime\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(baseUrl);
+    }
+
+    @Test
+    public void googleSearch() {
+        WebElement googleSearch = driver.findElement(By.name("q"));
+        //write textboxt
+        googleSearch.sendKeys("Interbank" + Keys.ENTER);
+        //find button
+        //WebElement searchButton = driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']"));
+      // WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]"));
+        //click button
+       // searchButton.click();
+    }
+
+    @AfterTest
+    public void endSession() {
+        //driver.quit();
+    }
+}
